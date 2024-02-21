6224dface2387ef2f8ce114206f62b1cbad3a6d4
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-02-21 13:14:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your code here
+var draggedElement = null;
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  draggedElement = ev.target;
+}
+
+function drop(ev, targetId) {
+  ev.preventDefault();
+  var targetElement = document.getElementById(targetId);
+  if (targetElement !== draggedElement) {
+    var temp = draggedElement.innerHTML;
+    draggedElement.innerHTML = targetElement.innerHTML;
+    targetElement.innerHTML = temp;
+  }
+}
